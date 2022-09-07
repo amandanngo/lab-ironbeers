3df20bc8d163af5c0a9ee60637940cfce5876b3d
Amanda Ngo <amanda.ngo172@gmail.com>
2022-09-07 14:02:18 -0500

    finished required iterations no styling or bonuses

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -21,5 +22,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req,res) =>{
+  
+  punkAPI 
+    .getBeers()
+    .then(beersFromApi => {
+      console.log(beersFromApi[0].image_url);
+      res.render('beers', {
+      allBeers: beersFromApi
+    });
+    })  
+   .catch(error => console.log(error));
+})
+
+app.get('/random-beer', (req,res) =>{
+  punkAPI
+    .getRandom()
+    .then(randBeer => {
+      res.render('random-beer', {
+        image: randBeer[0].image_url,
+        name: randBeer[0].name,
+        description: randBeer[0].description,
+        tagline: randBeer[0].tagline,
+        foodPairing: randBeer[0].food_pairing,
+        tips: randBeer[0].brewers_tips 
+      })
+      console.log(randBeer);
+    })
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
